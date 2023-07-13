973f5e043aba67f72b6ec7c321473e58a1401873
Shadab Syed <shadabsyed488@gmail.com>
2023-07-13 06:21:39 +0500

    2 new components About.jsx and Terms.jsx added,,, react router dom package installes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // components
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Routes>
+        <Route path="/" element={<HomeGuest />}></Route>
+        <Route path="/about-us" element={<About />}></Route>
+        <Route path="/terms" element={<Terms />}></Route>
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
