7f62221afec6cf300b0cf8914d3b36fe30a2373e
Shadab Syed <shadabsyed488@gmail.com>
2023-08-17 15:06:22 +0500

    video 23 completed , axios pkg installed

--- src/components/HomeGuest.jsx
+++ src/components/HomeGuest.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import Page from "./Page";
+import axios from "axios";
 
 function HomeGuest() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8080/register", {
+        username: "test2",
+        email: "test2@test.com",
+        password: "qwerty123456",
+      });
+      console.log("user was succesfully created");
+    } catch (e) {
+      console.log("there was an error");
+    }
+  }
   return (
     <>
       <Page wide={true} title="Welcome!">
@@ -16,7 +30,7 @@ function HomeGuest() {
             </p>
           </div>
           <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="form-group">
                 <label htmlFor="username-register" className="text-muted mb-1">
                   <small>Username</small>
